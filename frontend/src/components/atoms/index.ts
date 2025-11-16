a9b3d9493e6a4eac8151af42775fbdb733b9b9e4
aybeedee <abdullahumer575@gmail.com>
2025-11-16 20:55:36 +0500

    feat: setup shadcn, add base ui atoms

---
+++ frontend/src/components/atoms/index.ts
@@ -0,0 +1,14 @@
+export { Button } from "./button";
+export { Input } from "./input";
+export {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from "./dialog";
