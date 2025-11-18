8fc142156584b711982235675929171277b3a250
aybeedee <abdullahumer575@gmail.com>
2025-11-18 07:35:01 +0500

    chore: more accurate req user type

--- backend/src/lib/types.ts
+++ backend/src/lib/types.ts
@@ -2,5 +2,5 @@ import { Request } from "express";
 import { User } from "generated/prisma/client";
 
 export interface ReqWithUser extends Request {
-  user: User;
+  user: Pick<User, "id" | "username">;
 }
